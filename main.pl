85785151491bf0fbccaa8fad2cce91c82565a744
matilde-silva-21 <matilde.m.silva.21@gmail.com>
2022-12-20 14:34:46 +0000

    move done

--- main.pl
+++ main.pl
@@ -1,9 +1,9 @@
 :-[logic].
 
-/* Move = [Player, X, Y] 
+/* Move = [Player, X, Y] */
 move(GameState, Move, NewGameState) :-
     list_nth(Move, 0, Player),
     list_nth(Move, 1, X),
     list_nth(Move, 2, Y),
-    placeStone(GameState, Player, X, Y, NewGameState, 9).*/
+    placeStone(GameState, Player, X, Y, NewGameState, 9).
 
